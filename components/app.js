d4b4ed4847553343cf899d1d228bb6d795c2e02c
ckular <chirag.kular@matrixcare.com>
2023-06-08 11:09:06 -0500

    Initial Commit

---
+++ components/app.js
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+import Banner from "./banner";
+import HouseList from "./houseList";
+import House from "./house";
+import navValues from "../helpers/navValues";
+import React from "react";
+import ComponentPicker from './componentPicker';
+
+const navigationContext = React.createContext(navValues.home);
+
+const App = () => {
+  
+  const navigate = useCallback(
+    (navTo, param) => setNav({ current: navTo, param, navigate}),
+    []
+  )
+  const [nav, setNav] = useState( { current: navValues.home, navigate});
+
+  return (
+    <navigationContext.Provider value={nav}>
+      <Banner>
+        <div>Providing houses all over the world</div>
+      </Banner>
+      <ComponentPicker currentNavLocation={nav.current}/>
+    </navigationContext.Provider>
+  );
+};
+
+export { navigationContext };
+export default App;
