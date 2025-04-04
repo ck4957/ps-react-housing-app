510e2629681142bc07c9c864ceb9c9fd30e39ec0
Chirag Kular <kular_chirag@yahoo.com>
2025-04-04 01:06:44 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
+    <meta name="google-adsense-account" content="ca-pub-3911596373332918">
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
         />
         <link rel="stylesheet" href="css/globals.css" />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3911596373332918"
+     crossorigin="anonymous"></script>
       </Head>
       <body>
         <div className="container">
